c46eb6c4c02df2545c18c52ce908a8a0e9132ca7
Benjamin Chipinde <benjamin@kylix.online>
2022-06-07 01:38:09 +0300

    react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
-import { robots } from './robots'
 import './App.css';
 
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
+function App() {
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState('')
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setRobots(users))
+  },[]) 
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
   }
-  componentDidMount() {
-    this.setState({ robots })
-  }
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value})
-  }
-  render() {
-    const { robots, searchfield } = this.state
-    const filteredRobots = robots.filter(robot => {
+  const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-    })
+  })
+  console.log(searchfield, robots)
     if (!robots.length) {
       return <h1>Loading...</h1>
     }
     return (
       <div className="tc">
         <h1 className="f1">CHAPPIE</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots}/>
@@ -40,7 +37,6 @@ class App extends Component {
         </Scroll>
       </div>
     );
-  };
 }
 
 export default App;
